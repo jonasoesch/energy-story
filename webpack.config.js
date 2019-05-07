e320b23933798b4030c9617e56623fda194d4bf7
Jonas Oesch <jonas.oesch@fhnw.ch>
2019-05-07 15:03:30 +0200

    Configuration

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+// check environment mode
+var environment = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+function devProd(development, production) {
+    if(environment === "development") {
+        return development
+    } else {
+        return production 
+    }
+}
+
+module.exports = {
+    entry: {
+        demo:  './src/index.ts',
+    },
+    devtool: devProd('inline-source-map', false),
+    mode: devProd("development", "production"),
+    // Typescript
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.ts$/,
+                loader: 'string-replace-loader',
+                options: {
+                    search: "__API_URL__",
+                    replace: devProd("http://localhost:5000/", "https://www.cs.technik.fhnw.ch/lostintransition/logger/"),
+                }
+            }
+        ]
+
+    },
+    resolve: {
+        extensions: [ '.ts', '.js' ]
+    },
+    output: {
+        filename: '[name]/main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: './static'
+    },
+};
+
