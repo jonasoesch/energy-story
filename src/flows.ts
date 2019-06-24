68196efbc4da01f54453755fd7c8212e9fc82028
Jonas Oesch <jonas.oesch@fhnw.ch>
2019-06-24 11:38:16 +0200

    Eye-Tracking flows

--- src/flows.ts
+++ src/flows.ts
@@ -4,6 +4,10 @@ export const urlmap = {
     "middle": "https://www.cs.technik.fhnw.ch/lostintransition/envelope/middle/",
     "end": "https://www.cs.technik.fhnw.ch/lostintransition/envelope/end/",
 
+
+    "et": "https://www.cs.technik.fhnw.ch/lostintransition/envelope/et/",
+    "thanks": "https://www.cs.technik.fhnw.ch/lostintransition/envelope/thanks/",
+
     "MSSD": "https://www.cs.technik.fhnw.ch/lostintransition/mortality/SS/demographics.html",
     "MSSA":	"https://www.cs.technik.fhnw.ch/lostintransition/mortality/SS/absolute.html",
     "MSSR":	"https://www.cs.technik.fhnw.ch/lostintransition/mortality/SS/relative.html",
@@ -51,6 +55,15 @@ export const flows = {
     "subjType2": ["MJSD", "MJSA", "MJSR", "MJSC", "middle", "ESSA", "ESSB", "ESSC", "ESSD", "end"],
     "subjType3": ["MSSD", "MSSA", "MSSR", "MSSC", "middle", "EJSA", "EJSB", "EJSC", "EJSD", "end"],
     "subjType4": ["MJAD", "MJAA", "MJAR", "MJAC", "middle", "ESAA", "ESAB", "ESAC", "ESAD", "end"],
+
+    "et1": ["et", "MJAA", "MJAB", "MJSC", "MJSD", "ESAA", "ESAB", "ESSC", "ESSD", "thanks"],
+    "et2": ["et", "MJSA", "MJSB", "MJAC", "MJAD", "ESSA", "ESSB", "ESAC", "ESAD", "thanks"],
+    "et3": ["et", "MSAA", "MSAB", "MSSC", "MSSD", "EJAA", "EJAB", "EJSC", "EJSD", "thanks"],
+    "et4": ["et", "MSSA", "MSSB", "MSAC", "MSAD", "EJSA", "EJSB", "EJAC", "EJAD", "thanks"],
+    "et5": ["et", "EJAA", "EJAB", "EJSC", "EJSD", "MSAA", "MSAB", "MSSC", "MSSD", "thanks"],
+    "et6": ["et", "EJSA", "EJSB", "EJAC", "EJAD", "MSSA", "MSSB", "MSAC", "MSAD", "thanks"],
+    "et7": ["et", "ESAA", "ESAB", "ESSC", "ESSD", "MJAA", "MJAB", "MJSC", "MJSD", "thanks"],
+    "et8": ["et", "ESSA", "ESSB", "ESAC", "ESAD", "MJSA", "MJSB", "MJAC", "MJAD", "thanks"],
 }
 
 
